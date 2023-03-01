4c1728f1f79e416c243b1d84ca1fa7e44c5a152e
Sayemhaque <mdsayemofficial1000@gmail.com>
2023-03-02 00:05:01 +0600

    category section ui & fetch data done

--- js/main.js
+++ js/main.js
@@ -2,12 +2,24 @@ const  loadData = () => {
     const URL = "https://openapi.programming-hero.com/api/news/categories";
     fetch(URL)
     .then(res => res.json())
-    .then(data => displayCetegories(data.data))
+    .then(data => displayCetegories(data.data.news_category))
 }
 
 
 const displayCetegories = (data) => {
-  return console.log(data)
+  const cateContainer = document.getElementById("categories-container");
+
+  data.forEach((category) => {
+    cateContainer.innerHTML += `<a class="nav-link" href="#" onclick="fetchAllNews('${category.category_id}')">${category.category_name}</a>`
+  })
+
 }
 
-displayCetegories()
+
+const fetchAllNews = (categoryId) => {
+  const URL = `https://openapi.programming-hero.com/api/news/category/${categoryId}`
+  fetch(URL)
+  .then(res => res.json())
+  .then(data => console.log(data.data))
+  
+}
